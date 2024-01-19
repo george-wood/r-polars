b29a87d98fb724d46e9e7895ee787d46192f15ca
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-01-19 09:33:46 +0100

    init

--- altdoc/altdoc_postprocessing.R
+++ altdoc/altdoc_postprocessing.R
@@ -5,79 +5,12 @@ list_man_html = list.files("docs/man",
   recursive = TRUE
 )
 
-### Make the "Usage" section prettier (if there is one):
-### DataFrame_describe(...)   ->   <DataFrame>$describe()
-
-classes = c(
-  "Series", "DataFrame", "LazyFrame", "GroupBy",
-  "LazyGroupBy", "IO", "RField", "RThreadHandle", "SQLContext", "S3",
-  "Expr", "pl"
-)
-
-to_modify = grep(
-  paste0("/", paste(classes, collapse = "|")),
-  list_man_html,
-  value = TRUE
-)
-
-for (i in to_modify) {
-  which_class = gsub("docs/man/([^_]+).*$", "\\1", i, perl = TRUE)
+for (i in list_man_html) {
   orig = readLines(i, warn = FALSE)
-
-  if (!any(grepl("<h2 id=\"usage\">Usage</h2>", orig))) {
-    next
-  }
-
-  # IO functions are DataFrame or LazyFrame methods
-  if (which_class == "IO") {
-    if (any(grepl("<code class='language-R'>LazyFrame_sink", orig))) {
-      which_class <<- "LazyFrame"
-    } else if (any(grepl("<code class='language-R'>DataFrame_write", orig))) {
-      which_class <<- "DataFrame"
-    }
-  }
-
-  # prefix with pl$ for read/scan
-  if (which_class == "IO") {
-    which_input = if (any(grepl("<code class='language-R'>read_", orig))) {
-      "read"
-    } else if (any(grepl("<code class='language-R'>scan_", orig))) {
-      "scan"
-    } else {
-      ""
-    }
-    new = gsub(
-      paste0("<code class='language-R'>", which_input, "_"),
-      paste0("<code class='language-R'>pl$", which_input, "_"),
-      orig
-    )
-  } else if (which_class == "pl") {
-    new = gsub(
-      "<code class='language-R'>pl_",
-      "<code class='language-R'>pl$",
-      orig
-    )
-  } else if (which_class %in% c(
-    "ExprBin", "ExprCat", "ExprDT", "ExprList",
-    "ExprMeta", "ExprName", "ExprStr", "ExprStruct"
-  )) {
-    subns = tolower(gsub("Expr", "", which_class))
-
-    new = gsub(
-      paste0("<code class='language-R'>", which_class, "_"),
-      paste0("<code class='language-R'>&lt;Expr&gt;$", subns, "$"),
-      orig
-    )
-  } else {
-    new = gsub(
-      paste0("<code class='language-R'>", which_class, "_"),
-      paste0("<code class='language-R'>&lt;", which_class, "&gt;$"),
-      orig
-    )
-  }
-
   # fix escaping of left-angle brackets (not needed for right-angle brackets)
-  new = gsub("\\\\&lt;", "&lt;", new)
-
+  new = gsub("\\\\&lt;", "&lt;", orig)
+  new = gsub("\\\\&gt;", "&gt;", orig)
+  new = gsub("\\\\\\$", "$", new)
+  new = gsub("\\\\_", "_", new)
   writeLines(new, i)
 }
---
+++ temp.R
@@ -0,0 +1,103 @@
+#' Update the text in the usage section of an Rd file
+#'
+#' @param file path of an Rd file
+#' @param pattern Pattern to look for
+#' @param replacement Regex value
+#' @return The updated Rd file content invisibly
+#'
+#' @examples
+#' rd_usage_replace("man/DataFrame_clone.Rd", "DataFrame_", "<DataFrame>$")
+rd_usage_replace = function(file, pattern, replacement) {
+  content_rd = tools::parse_Rd(file)
+  tags = tools:::RdTags(content_rd)
+
+  which_usage = which(tags == r"(\usage)")
+  if (length(which_usage) > 0) {
+    content_rd[[which_usage]] = lapply(content_rd[[which_usage]], function(x) {
+      if (length(x) > 1) return(x)
+      attr_rd_tag = attr(x, "Rd_tag")
+      out = stringr::str_replace_all(x, pattern, replacement)
+      attr(out, "Rd_tag") = attr_rd_tag
+      out
+    })
+
+    content_rd |>
+      as.character(deparse = TRUE) |>
+      paste0(collapse = "") |>
+      brio::write_file(file)
+  } else {
+    return(invisible())
+  }
+}
+
+#' Update all usage sections in Rd files
+#' @param files A character vector of Rd files to update. If NULL, all Rd files in the package are updated.
+#' @return NULL
+#' @examples
+#' rd_usage_update()
+rd_usage_update = function(files = NULL) {
+  files = files %||% list.files("man", pattern = r"(.*\.Rd$)", full.names = TRUE, recursive = TRUE)
+
+  patterns = tibble::tribble(
+    ~pattern, ~replacement,
+    "DataFrame_", "<DataFrame>$",
+    "DynamicGroupBy_", "<DynamicGroupBy>$",
+    "Expr_", "<Expr>$",
+    "ExprBin_", "<Expr>$bin$",
+    "ExprCat_", "<Expr>$cat$",
+    "ExprDT_", "<Expr>$dt$",
+    "ExprList_", "<Expr>$list$",
+    "ExprMeta_", "<Expr>$meta$",
+    "ExprName_", "<Expr>$name$",
+    "ExprStr_", "<Expr>$str$",
+    "ExprStruct_", "<Expr>$struct$",
+    "GroupBy_", "<GroupBy>$",
+    "(IO|pl)_read_", "pl$read_", # file names are "IO_read_", function names are "pl_read_"
+    "(IO|pl)_scan_", "pl$scan_", # file names are "IO_scan_", function names are "pl_scan_"
+    "(IO|LazyFrame)_sink_", "<LazyFrame>$sink_", # file names are "IO_sink_", function names are "LazyFrame_sink_"
+    "(IO|DataFrame)_write_", "<DataFrame>$write_", # file names are "IO_write_", function names are "DataFrame_write_"
+    "LazyFrame_", "<LazyFrame>$",
+    "LazyGroupBy_", "<LazyGroupBy>$",
+    "pl_", "pl$",
+    "RField_", "<RField>$",
+    "RThreadHandle_", "<RThreadHandle>$",
+    "Series_", "<Series>$",
+    "SQLContext_", "<SQLContext>$",
+  )
+
+  purrr::walk2(
+    patterns$pattern, patterns$replacement,
+    \(pattern, replacement) {
+      files |>
+        stringr::str_subset(pattern) |>
+        purrr::walk(
+          \(file) {
+            print(file)
+            tryCatch(
+              rd_usage_replace(file, pattern, replacement),
+              error = function(e) {
+                print(e)
+                cat("Error updating usage section in ", file, "\n", sep = "")
+              }
+            )
+          }
+        )
+    }
+  )
+}
+
+# TODO: remove after R 4.4 released
+`%||%` = function(x, y) {
+  if (is.null(x)) y else x
+}
+
+
+
+future::plan(future::multicore);
+fs::dir_copy("man", "man_old")
+rd_usage_update()
+source("altdoc/altdoc_preprocessing.R");
+altdoc::render_docs(freeze = TRUE, parallel = TRUE, verbose = TRUE);
+source("altdoc/altdoc_postprocessing.R");
+fs::dir_delete("man")
+fs::file_move("man_old", "man")
